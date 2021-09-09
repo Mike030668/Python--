ba19a0b8eee53d188bb10ed17fefa16e391fd39c
Михаил Пузицкий <gretawolf@live.ru>
2021-09-10 00:50:11 +0300

    Pro

--- lesson_2_Pro.py
+++ lesson_2_Pro.py
@@ -1,4 +1,40 @@
 # 6. Найти сумму цифр числа.
-fig = input('введите цифру')
+# 7. Найти сумму цифр числа.
+fig = input('введите число')
+summma = 0
+prod = 1
 for el in fig:
-    print(el)
+    summma += int(el)
+    prod *= int(el)
+print('сумма цифр числа ' + fig + ' = ' + str(summma))
+print('произведение цифр числа ' + fig + ' = ' + str(prod))
+
+# 8. Дать ответ на вопрос: есть ли среди цифр числа 5?
+fig = input('введите число')
+check_fig = '5'
+i = 0
+for el in fig:
+    if el == check_fig:
+        print("Есть число " + check_fig + ' в ' + fig)
+        break
+    else:
+        if i == len(fig) - 1:
+            print("Нет числа " + check_fig + ' в ' + fig)
+    i += 1
+
+# 9. Найти максимальную цифру в числе
+fig = input('введите число')
+max_num = 0
+for el in fig:
+    if int(el) > max_num:
+        max_num = int(el)
+print('В числе ' + fig + ' макс.число ', max_num)
+
+# 10. Найти количество цифр 5 в числе
+fig = input('введите число')
+check_fig = '5'
+qty = 0
+for el in fig:
+    if el == check_fig:
+        qty += 1
+print('В числе ' + fig + ' ' + check_fig + ' - ' + str(qty) + ' раз')
