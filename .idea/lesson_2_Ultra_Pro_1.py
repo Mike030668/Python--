b1c506106620a88f0b2e8d244588475d4d61f1a1
Михаил Пузицкий <gretawolf@live.ru>
2021-09-10 21:19:29 +0300

    Ultra_Pro Вариант когда загадывает программа, а отгадывает пользователь

---
+++ .idea/lesson_2_Ultra_Pro_1.py
@@ -0,0 +1,22 @@
+import random
+attempt = 0
+chances = 10
+print('Начнем игру угадай число?')
+play = input('если да, то поставь + или - :')
+
+if play == '+':
+    print('Ура!! Ты в игре')
+    print('У тебя ' + str(chances) + ' попыток')
+    print('я загадал число от 0 до 100')
+    number = random.randint(0, 100)
+
+    while attempt < 10:
+        figure = int(input('Напиши свое число '))
+        attempt += 1
+        if figure < number: print('Загадано больше твоего числа ')
+        if figure > number: print('Загадано меньше твоего числа ')
+        if figure == number: break
+    if figure == number: print('Отлично! Число угадано с ' + str(attempt) + ' раз')
+    if figure != number: print('Упс, пролет, было загадано число' + str(attempt))
+
+else: print('Жаль, что ты сегодня не в духе ((')
