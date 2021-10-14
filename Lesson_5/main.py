47beb10f422bc1679e126426e4067d2f2943de56
Михаил Пузицкий <gretawolf@live.ru>
2021-10-14 20:37:07 +0300

    готово

--- Lesson_5/main.py
+++ Lesson_5/main.py
@@ -1,6 +1,7 @@
 import divisor_master as dm
 import random
 
+# функция тестирования
 def test_function(fun, test_lst):
     good_test = 0
     bad_test = 0
@@ -8,10 +9,10 @@ def test_function(fun, test_lst):
 
     for value in test_lst:
         try:
-            res = fun(value)
-            bad_test += 1
-        except:
-            print('Я нашел не число!')
+            fun(value)
+            good_test += 1
+        except Exception as e:
+            print(e)
             bad_values.append(value)
             bad_test += 1
 
@@ -23,26 +24,37 @@ def test_function(fun, test_lst):
     return bad_values
 
 
-
-test_lst_1 = [random.randint(1, 1000) for i in range(100)]
-test_lst_2 = [random.randrange(1, 1000, 3) for i in range(100)]
+# тестовые наборы
+test_lst_1 = [random.randint(1, 100) for i in range(100)]
+test_lst_2 = [random.randrange(1, 100, 3)*(-1)**i for i in range(100)]
 test_lst_3 = [random.random()*100 for i in range(100)]
 
 p = [1, 0.1, 'r', 5, 10, 23.6, '34']
 test_lst_4 = [random.choice(p) for i in range(100)]
 
+# набор тестов
 big_test = [test_lst_1, test_lst_2, test_lst_3, test_lst_4]
-one_test = random.choice(big_test)
 
-print(f'Тест на наборе {one_test}')
+# смешанный тест
+mix_test = []
+for test in big_test:
+    mix_test.extend(test)
 
+# смешанный тест случайных 20
+mix_test = random.sample(mix_test, 20)
+
+print(f'Тест на наборе {mix_test}')
+
+# список тестируемых функций
 funlst = [dm.find_deviders,
           dm.simple_multipliers,
           dm.show_simpledeviders,
           dm.find_deviders]
 
+# проход случайным тестом по функциям
 for fun in funlst:
-    print(f'функция {fun}')
-    check = test_function(fun, one_test)
-    print(check)
+    print(f'функция {fun.__name__}')
+    check = test_function(fun, mix_test)
+    print(f'Значения дающие ошибку {check}')
+    print()
 
