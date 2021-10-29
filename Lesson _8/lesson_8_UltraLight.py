b4335c7cf1faa1b9d02e722312155062b560f468
Михаил Пузицкий <gretawolf@live.ru>
2021-10-29 22:27:48 +0300

    готово

--- Lesson _8/lesson_8_UltraLight.py
+++ Lesson _8/lesson_8_UltraLight.py
@@ -9,28 +9,27 @@ a = b // c if b % c == 0 else b % c
 print(c)
 print(b)
 print(a)
+print()
 
 # Генераторы последовательностей
-
 a = [np.sin(x/2*np.pi) + np.cos(x/2*np.pi) for x in range(100)]
 print(a)
+print()
 
 # декоратор
 def split_sings(f):
     def checker(*args, **kwargs):
         data = f(*args, **kwargs)
         plus = list(filter(lambda x: x > 0, data))
-        # assert isinstance(data, object)
         minus = list(filter(lambda x: x < 0, data))
-        return plus, minus
+        return (plus, minus)
     return checker
 
 @split_sings
-def get_rand(num):
-    data = np.random.standard_normal(num)*np.random.gumbel(num)
+def get_randdata(num):
+    data = np.random.standard_normal(num) * np.random.gumbel(num)
     return data
 
-plus, minus = get_rand(20)
+data = get_randdata(20)
 
-print(plus)
-print(minus)
+print(data)
