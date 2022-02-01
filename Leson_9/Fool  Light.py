b183125db09b634991b53b141b60103d3f4b3778
Михаил Пузицкий <gretawolf@live.ru>
2022-02-01 16:25:49 +0300

    версия Light

--- Leson_9/Fool  Light.py
+++ Leson_9/Fool  Light.py
@@ -3,6 +3,14 @@ import numpy as np
 import pandas as pd
 from BaseFool import Durack
 
+"""
+Количество карт, людей и роботов уже задано.
+Вам нужно указать имя при опросе
+Далее нужно смотреть на сообщения и отвечать на вопросы в игре
+
+Вы сыграете со случайным роботом,
+"""
+
 # Инициализация игры
 game = Durack(card4plaer=6, humans=1, robots=1)
 gaimers, playcoloda, kozir = game.init_game()
