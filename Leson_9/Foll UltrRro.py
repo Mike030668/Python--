fa57e3e0f168c94265baf58320baca780e2c5ba3
Михаил Пузицкий <gretawolf@live.ru>
2022-02-01 16:21:06 +0300

    версия Ultra Pro

---
+++ Leson_9/Foll UltrRro.py
@@ -0,0 +1,23 @@
+import random
+import numpy as np
+import pandas as pd
+from BaseFool import Durack
+
+"""
+Пройдите опрос перед игрой
+- укажите количество карт выдаваемых на руки
+- укажите оличество людей, роботов
+- Дайте имена людям
+
+- если в игоре люди, то нужно смотреть на сообщения
+  и отвечать на вопросы в игре
+
+"""
+
+# Инициализация игры
+game = Durack()
+gaimers, playcoloda, kozir = game.init_game()
+
+# Запуск игрового цикла
+game.go_game(gaimers, playcoloda, kozir)
+
