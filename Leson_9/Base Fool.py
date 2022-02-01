cef95eb7c5653e8186ff1adcdd370baf984e692f
Михаил Пузицкий <gretawolf@live.ru>
2022-02-01 14:11:54 +0300

    ok

---
+++ Leson_9/Base Fool.py
@@ -0,0 +1,12 @@
+import random
+import pandas
+
+class Fool():
+    """
+    База для игры в дурака
+    """
+    def koloda(self):
+        musty = ['Черви', 'Пики', 'Крести', 'Буби']
+        typecards = [6,7,8,9,10,'Валет', "Дама",'Король','Туз']
+        koloda =
+
