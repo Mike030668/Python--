076a835afb9a073f3c5c28fe1ab34206fa94e0b1
Михаил Пузицкий <gretawolf@live.ru>
2022-02-01 16:23:14 +0300

    версия Pro

--- Leson_9/Fool Pro.py
+++ Leson_9/Fool Pro.py
@@ -3,6 +3,14 @@ import numpy as np
 import pandas as pd
 from BaseFool import Durack
 
+"""
+Количество карт, людей и роботов уже задано
+
+Перед вами сыграют робаты со случайной инициализацией
+типа игры, см. сообщение внвчале
+
+"""
+
 # Инициализация игры
 game = Durack(card4plaer=6, humans=0, robots=4)
 gaimers, playcoloda, kozir = game.init_game()
